f1e50bc6b23525a7a96c531dcdf7f358dbc7398a
floorup1 <42138394+floorup1@users.noreply.github.com>
2021-10-14 07:43:20 +0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -10,14 +10,6 @@ sudo rm -rf startpage
 sudo curl -LOk https://github.com/floorup1/startpage/archive/master.zip
 unzip -o master.zip
 sudo cp -r startpage-master startpage
-if [ -d $HOME/kiosk ] ; then
-cd $HOME/kiosk
-git pull
-cd $HOME
-else
-cd $HOME
-git clone https://github.com/floorup1/kiosk.git
-fi
 xset -dpms
 xset s off
 openbox-session &
@@ -27,5 +19,13 @@ while true; do
 sudo curl -o /etc/chromium/policies/managed/policy.json https://raw.githubusercontent.com/floorup1/kiosk/master/policy.json
 rm -rf /home/kiosk-user/.config/chromium/*
 rm -rf /home/kiosk-user/.cache/chromium/*
+if [ -d $HOME/kiosk ] ; then
+cd $HOME/kiosk
+git pull
+cd $HOME
+else
+cd $HOME
+git clone https://github.com/floorup1/kiosk.git
+fi
 chromium --start-maximized 'file:///home/kiosk-user/startpage/start.html'
 done
