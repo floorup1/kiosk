f77e2efb733435dd8298945b15d6e3069076540f
floorup1 <42138394+floorup1@users.noreply.github.com>
2021-10-14 07:52:24 +0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo curl -o /opt/kiosk.sh https://raw.githubusercontent.com/floorup1/kiosk/master/kiosk.sh
 sudo curl -o /etc/sudoers https://raw.githubusercontent.com/floorup1/kiosk/master/sudoers
-sudo apt install acpid mc at git screen -y
+sudo apt install acpid mc at git screen htop -y
 sudo rm -rf /home/kiosk-user/share/*
 sudo rm -rf startpage
 sudo curl -LOk https://github.com/floorup1/startpage/archive/master.zip
