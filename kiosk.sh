51169beb4a9049171f663bcc5b36f21a7527f3c7
floorup1 <42138394+floorup1@users.noreply.github.com>
2023-09-26 14:46:29 +0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo curl -o /opt/kiosk.sh https://raw.githubusercontent.com/floorup1/kiosk/master/kiosk.sh
-sudo curl -o /etc/sudoers https://raw.githubusercontent.com/floorup1/kiosk/master/sudoers
 sudo rm -rf /home/kiosk-user/share/*
 sudo rm -rf /home/kiosk-user/Downloads/*
 sudo rm -rf /home/kiosk-user/Загрузки/*
@@ -21,6 +19,9 @@ if [ "$K2" -eq "0" ]; then
 	sudo sh -c "echo '*;*;*;MoTuThFr0700-1900|We0700-2000|Sa0700-1600' >> /etc/security/time.conf"
 fi
 sudo curl -o /etc/chromium/policies/managed/policy.json https://raw.githubusercontent.com/floorup1/kiosk/master/policy.json
+sudo curl -o /opt/kiosk.sh https://raw.githubusercontent.com/floorup1/kiosk/master/kiosk.sh
+sudo curl -o /etc/sudoers https://raw.githubusercontent.com/floorup1/kiosk/master/sudoers
+sudo curl -o /opt/schedule.cronjob
 rm -rf /home/kiosk-user/.config/chromium/*
 rm -rf /home/kiosk-user/.cache/chromium/*
 if [ -d $HOME/startpage ] ; then
