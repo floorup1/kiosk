6a40681c8a20f489e32302863c636f51d6c76f15
floorup1 <42138394+floorup1@users.noreply.github.com>
2023-09-26 14:42:22 +0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -3,14 +3,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo curl -o /opt/kiosk.sh https://raw.githubusercontent.com/floorup1/kiosk/master/kiosk.sh
 sudo curl -o /etc/sudoers https://raw.githubusercontent.com/floorup1/kiosk/master/sudoers
-K1=$(grep -c "account required pam_time.so" /etc/pam.d/login)
-if [ "$K1" -eq "0" ]; then
-	sudo sh -c "echo 'account required pam_time.so' >> /etc/pam.d/login"
-fi
-K2=$(grep -c "*;*;*;MoTuThFr0700-1900|We0700-2000|Sa0700-1600" /etc/security/time.conf)
-if [ "$K2" -eq "0" ]; then
-	sudo sh -c "echo '*;*;*;MoTuThFr0700-1900|We0700-2000|Sa0700-1600' >> /etc/security/time.conf"
-fi
+
 sudo rm -rf /home/kiosk-user/share/*
 sudo rm -rf /home/kiosk-user/Downloads/*
 sudo rm -rf /home/kiosk-user/Загрузки/*
@@ -20,6 +13,14 @@ openbox-session &
 start-pulseaudio-x11
 
 while true; do
+K1=$(grep -c "account required pam_time.so" /etc/pam.d/login)
+if [ "$K1" -eq "0" ]; then
+	sudo sh -c "echo 'account required pam_time.so' >> /etc/pam.d/login"
+fi
+K2=$(grep -c "*;*;*;MoTuThFr0700-1900|We0700-2000|Sa0700-1600" /etc/security/time.conf)
+if [ "$K2" -eq "0" ]; then
+	sudo sh -c "echo '*;*;*;MoTuThFr0700-1900|We0700-2000|Sa0700-1600' >> /etc/security/time.conf"
+fi
 sudo curl -o /etc/chromium/policies/managed/policy.json https://raw.githubusercontent.com/floorup1/kiosk/master/policy.json
 rm -rf /home/kiosk-user/.config/chromium/*
 rm -rf /home/kiosk-user/.cache/chromium/*
