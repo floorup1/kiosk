8bcd258a760500509478efd2cc402e3b16f31da3
floorup1 <42138394+floorup1@users.noreply.github.com>
2024-04-05 07:29:02 +0400

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+apt update
+apt full-upgrade -y
+apt install acpid mc pcmanfm openbox lightdm xorg curl git ansible -y
 curl -o $HOME/autostart.sh https://raw.githubusercontent.com/floorup1/kiosk/master/autostart.sh
 curl -o $HOME/ans.sh https://raw.githubusercontent.com/floorup1/kiosk/master/ans.sh
+curl -o /home/kiosk-user/.config/openbox/autostart https://raw.githubusercontent.com/floorup1/kiosk/master/autostart
+curl -o /home/kiosk-user/.config/openbox/rc.xml https://raw.githubusercontent.com/floorup1/kiosk/master/rc.xml
+chmod +x $HOME/autostart.sh
 chmod +x $HOME/ans.sh
+chmod ugo+rwx /home/kiosk-user/.config/openbox/autostart
+chmod ugo+rw /home/kiosk-user/.config/openbox/rc.xml
 curl -o $HOME/schedule.cronjob  https://raw.githubusercontent.com/floorup1/kiosk/master/schedule.cronjob
 crontab $HOME/schedule.cronjob
 if [ -d /etc/chromium/policies/managed ]; then
